cc1f575aeb083749967770f7d240c1d5a9b0474c
DEEPAK N <iamdeepak89000@gmail.com>
2025-01-23 20:13:52 +0530

    greatest number and less there number find out

---
+++ num.java
@@ -0,0 +1,20 @@
+import java.unit.scanner;
+class num {
+public static void main (String [] args)
+{
+scanner sc = new scanner(System.in);
+int a = sc.nextInt();
+if (a<50)
+{
+System.out.println("Greatest number 50");
+}
+else if(a>40 && a<50) 
+{
+System.out.println("bellowing the number 40 to 50 number");
+}
+else 
+{
+System.out.println("less then number");
+}
+}
+}
