00d59a8b15864df836cc05bbdc2319d56cdb157d
DEEPAK N <iamdeepak89000@gmail.com>
2025-04-16 20:28:32 +0530

    odd and even number

---
+++ evenodd.java
@@ -0,0 +1,13 @@
+class even{
+    public static void main(String args[])
+    {
+        int i=13;
+        if (i % 2 == 0){
+            System.out.println("it is even number");
+        }else {
+            System.out.println("it is odd number");
+        }
+        
+
+    }
+}
